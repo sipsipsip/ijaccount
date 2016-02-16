682454b2311381acfe798de6a69c5bfc9635983a
Muhammad Azamuddin <azamuddin@live.com>
2016-02-16 14:54:20 +0700

    UPDATE: CRUD Controller now support UPDATE
    Methods: CREATE (ADD previously) - UPDATE
    todos: DELETE

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -76,5 +76,84 @@ class CRUDController extends Controller {
 
 
 
+	/** add new Model **/
+     	public function getUpdate($id){
+     	    \Eloquent::unguard();
+     	    $input = \Input::except(['model', 'btm', 'hm']);
+
+             // Get the query params
+             /** belongsTo relationship automatically handled here **/
+             $modelClass = \Input::get('model');
+             $modelClass = ucfirst($modelClass);
+             $modelClass = 'App\\Models\\'.$modelClass;
+
+
+
+             $result = [];
+
+             // find the object edited
+             // if not found exit early
+             if(!$modelClass::find($id)){
+                $result['message'] = 'Model not found!';
+                $result['error'] = TRUE;
+                $result['success'] = FALSE;
+                return $result;
+             }
+
+             $modelClass = $modelClass::find($id);
+
+             if($objectModel = $modelClass->update($input)){
+
+                 /** handle belongsToMany relationship **/
+                 if(\Input::get('btm')){
+                     // btm is belongsToMany, the value is model of belong and its ids
+                     $btm = \Input::get('btm');
+                     foreach($btm as $owner => $ids){
+                         $objectModel->$owner()->sync($ids);
+                     }
+                 }
+
+
+                 /** handle hasMany relationship **/
+                 /** this one is only enhanced feature right now,
+                     making it possible to add new related object at the same time of
+                     parent creation **/
+                 /** most of the time when you want to add new has many object, you'll do it
+                     using belongsTo which is standard, just specify the id of the parent as
+                     params **/
+
+                 if(\Input::get('hm')){
+                     // hm stands for hasMany, the value is models of belonging and its ids
+                     $hm = \Input::get('hm');
+                     $rels = [];
+                     foreach($hm as $owned => $newhasmanyobject){
+                         foreach($newhasmanyobject as $obj){
+                            $relatedModel = 'App\\Models\\'.$owned;
+                            $o = new $relatedModel($obj);
+                            $rels[] = $o;
+                         }
+
+                         // delete already attached has many models
+                         $objectModel->$owned()->delete();
+
+                         // save related has many models
+                         $objectModel->$owned()->saveMany($rels);
+                     }
+
+                 }
+
+                 $result['message'] = 'Berhasil mengupdate '.\Input::get('model');
+                 $result['error'] = FALSE;
+                 $result['success'] = TRUE;
+                 return $result;
+             } else {
+                 $result['message'] = 'Terjadi kesalahan! '.\Input::get('model');
+                 $result['error'] = TRUE;
+                 $result['success'] = FALSE;
+                 return $result;
+             }
+     	}
+
+
 
 }
