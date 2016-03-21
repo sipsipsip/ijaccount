5034fceacb4805d0d39ef3538d3a782edc4b02fa
Muhammad Azamuddin <azamuddin@live.com>
2016-03-21 16:54:10 +0700

    change controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,6 +65,7 @@ class AuthController extends Controller {
 
 //        $response = $client->get('http://localhost:3000/kantor/pola-karir/public/remote-logout');
         ( $client->get('http://apps-itjen.kemenkeu.go.id/staging/talent/public/remote-logout') );
+        file_get_contents('http://apps-itjen.kemenkeu.go.id/staging/talent/public/remote-logout');
         \Auth::logout();
         return \Redirect::to('/');
 	}
