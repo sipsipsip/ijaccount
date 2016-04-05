4afbc15dd11d69509225c04fa0b4643141a6e7e6
Muhammad Azamuddin <azamuddin@live.com>
2016-04-05 16:31:09 +0700

    Adding beta tag on header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,9 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{url('/')}}">APPS ITJEN</a>
+				<a class="navbar-brand" href="{{url('/')}}">APPS ITJEN 
+                    <span style="position:absolute" class="label label-danger">beta</span>
+                </a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
